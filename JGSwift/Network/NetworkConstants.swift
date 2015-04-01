22859e291503b6cf65395bff2b2e92a8cf9104cf
JULIEN GOUDET <julien.goudet@atos.net>
2015-04-01 14:15:52 +0200

    [ADD] Carthage for Alamofire and Argo

---
+++ JGSwift/Network/NetworkConstants.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkConstants.swift
+//  JGSwift
+//
+//  Created by Julien Goudet on 01/04/2015.
+//  Copyright (c) 2015 JulienGdt. All rights reserved.
+//
+
+import Foundation
+
+//import Argo 
